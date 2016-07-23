b4f91a7c3499a1e899cc06feabd457347c327ae6
tstomoki <tstomoki4@gmail.com>
2016-07-24 01:40:50 +0900

    added gitignore and tig installation command

--- scripts/init.sh
+++ scripts/init.sh
@@ -14,10 +14,32 @@ git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 # install rbenv
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 
-
 # config git
 git config --global user.name "tstomoki"
 git config --global user.email tstomoki4@gmail.com
+
+# install tig
+if [ -e $PWD/lib ]; then
+    # 存在する場合
+    echo "lib dir already exists"
+else
+    # 存在しない場合
+    mkdir lib
+fi
+cd lib
+
+if [ -e tig-2.1.tar.gz ]; then
+    # 存在する場合
+    echo "file already exists"
+else
+    # 存在しない場合
+    wget http://jonas.nitro.dk/tig/releases/tig-2.1.tar.gz
+fi
+tar xvzf tig-2.1.tar.gz
+cd tig-2.1
+./configure
+make 
+sudo make install
 
 # install tmux
 sudo yum -y install tmux
