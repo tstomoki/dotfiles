023e25f62284538f5862be55bc92268370c29df1
tstomoki <tstomoki4@gmail.com>
2016-08-28 23:04:26 +0900

    updated init files

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,9 +1,31 @@
+(setq gnutls-min-prime-bits 1024)
 ;; load all files in ~/.emacs.d/site-lisp
+;; ~/.emacs.d/site-lisp 以下全部読み込み
 (let ((default-directory (expand-file-name "~/.emacs.d/site-lisp")))
   (add-to-list 'load-path default-directory)
-  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-      (normal-top-level-add-subdirs-to-load-path)))
+    (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+          (normal-top-level-add-subdirs-to-load-path)))
+          (require 'init-loader)
+          (setq init-loader-show-log-after-init nil)
+          (init-loader-load "~/.emacs.d/inits")
 
-(require 'init-loader)
-(setq init-loader-show-log-after-init nil)
-(init-loader-load "~/.emacs.d/inits")
+(require 'git-gutter)
+(global-git-gutter-mode t)
+
+(require 'python)
+(defun turn-on-flycheck-mode ()
+  (flycheck-mode 1))
+(add-hook 'python-mode-hook 'turn-on-flycheck-mode)
+
+;;
+;; Auto Complete
+;;
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
+(add-to-list 'ac-modes 'fundamental-mode)  ;; fundamental-mode
+(add-to-list 'ac-modes 'org-mode)
+(add-to-list 'ac-modes 'yatex-mode)
+(ac-set-trigger-key "TAB")
+(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-fuzzy t)          ;; 曖昧マッチ
