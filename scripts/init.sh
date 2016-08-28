ae4583bb03a63447b8662735440108e446e59bf4
tstomoki <tstomoki4@gmail.com>
2016-08-28 23:03:23 +0900

    updated for emacs without x systems

--- scripts/init.sh
+++ scripts/init.sh
@@ -60,7 +60,7 @@ source ~/.zshrc
 wget http://ftp.jaist.ac.jp/pub/GNU/emacs/emacs-24.5.tar.gz
 tar xzvf emacs-24.5.tar.gz
 cd emacs-24.5
-./configure
+./configure --without-x
 make
 sudo make install
 
