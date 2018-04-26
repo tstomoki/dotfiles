6022de0af6d6dc1410c22339f1676fc632a54264
tstomoki <tstomoki4@gmail.com>
2018-04-26 10:15:26 +0900

    add emacs color and indent mode

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -29,3 +29,18 @@
 (ac-set-trigger-key "TAB")
 (setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
 (setq ac-use-fuzzy t)          ;; 曖昧マッチ
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(font-lock-function-name-face ((t (:foreground "color-39")))))
+
+(electric-indent-mode 0)
