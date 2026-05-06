d03ccc614f9f2f1308e3a89e9a45ba323046b1cb
tstomoki <tstomoki4@gmail.com>
2026-05-06 11:18:18 +0900

    emacs: git-gutterとauto-completeの設定を削除

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -9,33 +9,17 @@
           (setq init-loader-show-log-after-init nil)
           (init-loader-load "~/.emacs.d/inits")
 
-(require 'git-gutter)
-(global-git-gutter-mode t)
-
 (require 'python)
 (defun turn-on-flycheck-mode ()
   (flycheck-mode 1))
 (add-hook 'python-mode-hook 'turn-on-flycheck-mode)
-
-;;
-;; Auto Complete
-;;
-(require 'auto-complete-config)
-(ac-config-default)
-(add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
-(add-to-list 'ac-modes 'fundamental-mode)  ;; fundamental-mode
-(add-to-list 'ac-modes 'org-mode)
-(add-to-list 'ac-modes 'yatex-mode)
-(ac-set-trigger-key "TAB")
-(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
-(setq ac-use-fuzzy t)          ;; 曖昧マッチ
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages '(flycheck)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
